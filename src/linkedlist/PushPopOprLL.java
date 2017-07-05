7d378493b0de4eaf6f1cc87c9f3eec77616466c6
jaswal116 <jaswal116@gmail.com>
2017-07-05 00:30:28 -0400

    Update PushPopOprLL.java

--- src/linkedlist/PushPopOprLL.java
+++ src/linkedlist/PushPopOprLL.java
@@ -9,19 +9,17 @@ public class PushPopOprLL {
 
 	public static void main(String[] args) {
 		LinkedList<String> arrl = new LinkedList<String>();
-        arrl.add("First");
-        arrl.add("Second");
-        arrl.add("Third");
-        arrl.add("Random");
-        System.out.println(arrl);
-        arrl.push("push element");
-        System.out.println("After push operation:");
-        System.out.println(arrl);
-        arrl.pop();
-        System.out.println("After pop operation:");
-        System.out.println(arrl);
-		
-		
+        	arrl.add("First");
+        	arrl.add("Second");
+       		arrl.add("Third");
+        	arrl.add("Random");
+        	System.out.println(arrl);
+        	arrl.push("push element");
+        	System.out.println("After push operation:");
+        	System.out.println(arrl);
+        	arrl.pop();
+        	System.out.println("After pop operation:");
+        	System.out.println(arrl);
 		
 	}
 

367a2dff267bf1046945dd583e406a7770d8a1c6
jaswal116 <jaswal116@gmail.com>
2017-07-26 08:43:24 -0400

    Rename src/WriteFile.java to src/ioOperations/WriteFile.java

--- src/ioOperations/WriteFile.java
+++ src/ioOperations/WriteFile.java
@@ -14,7 +14,7 @@ public class WriteFile {
 fileSeparator + "Users" + fileSeparator + "jj" + fileSeparator + "Desktop" + fileSeparator + "test_abs.txt" ;
 		File fileNew = new File(absoluteFilePath);
 		fileNew.createNewFile();
-		String filedata = " While creating the file path, we should use System property file.separator to make our program platform independent. Let’s see different scenarios with a simple java program to create new file in java.";
+		String filedata = " While creating the file path, we should use System property file.separator to make our program platform independent. Letâ€™s see different scenarios with a simple java program to create new file in java.";
 		FileOutputStream fos = new FileOutputStream(absoluteFilePath);
 		fos.write(filedata.getBytes());
 		fos.flush();

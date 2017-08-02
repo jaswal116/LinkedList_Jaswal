75a5afb23553c7bbd6425c725717a53d978a75d8
jaswal116 <jaswal116@gmail.com>
2017-08-02 09:24:47 -0400

    Add files via upload

---
+++ src/StringWork/PrintAlphabets.java
@@ -0,0 +1,11 @@
+package stringwork;
+
+public class PrintAlphabets {
+
+	public static void main(String[] args) {
+		System.out.println("Alphabets are : ");
+		for(char c = 'a';c<='z';c++){
+			System.out.print(c+" ");
+		}
+	}
+}
